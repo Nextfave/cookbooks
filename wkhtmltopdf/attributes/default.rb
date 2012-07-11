0491cf4192375000889311d138cddfe6ff907579
Michael Solovyov <spinalfluid@gmail.com>
2012-07-11 16:06:19 -0700

    Update master

--- wkhtmltopdf/attributes/default.rb
+++ wkhtmltopdf/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:wkhtmltopdf] = {}
 default[:wkhtmltopdf][:version] = '0.9.9'
 default[:wkhtmltopdf][:arch] = 'i386'
-default[:wkhtmltopdf][:binary] = '/usr/local/bin/wkhtmltopdf'
+default[:wkhtmltopdf][:binary] = '/usr/bin/wkhtmltopdf'
